879e809ed18076106f9543142972a7553d2f8eec
Yaroslav1992-art <Lotslav@gmail.com>
2020-01-23 23:13:20 +0200

    Updates

---
+++ precache-manifest.31828b9e431630404e078e99f0bea2ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187b12141c4d97ae83c448c7adcde9de",
+    "url": "/PhoneBook-redux/index.html"
+  },
+  {
+    "revision": "d03382587daf54981cd9",
+    "url": "/PhoneBook-redux/static/css/main.196d98a1.chunk.css"
+  },
+  {
+    "revision": "c273933e41a8b5f8f687",
+    "url": "/PhoneBook-redux/static/js/2.2eaeebfb.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/PhoneBook-redux/static/js/2.2eaeebfb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d03382587daf54981cd9",
+    "url": "/PhoneBook-redux/static/js/main.ba2763e1.chunk.js"
+  },
+  {
+    "revision": "47759567f988d4cf5dff",
+    "url": "/PhoneBook-redux/static/js/runtime-main.8fc8f612.js"
+  }
+]);
